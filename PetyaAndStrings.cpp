eec97b4deec315399c883ced3a5214ad33e87ef6
Tanim1440 <mtanimkhan520@gmail.com>
2025-07-25 02:25:15 +0600

    my tweleveth dsa solve

--- PetyaAndStrings.cpp
+++ PetyaAndStrings.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include <algorithm> 
+#include<algorithm> 
 using namespace std;
 int main(){
     string s1,s2;
@@ -9,25 +9,64 @@ int main(){
     transform(s2.begin(),s2.end(),s2.begin(),::tolower);
     sort(s1.begin(),s1.end());
     sort(s2.begin(),s2.end());
-    cout<<s1<<endl<<s2<<endl;
-    for(int i=0;s1[i]!='\0';i++){
-        if(s1[i]!=s2[i]){
-            if(s1[i]>s2[i]){
-              a+=s1[i];
+    //cout<<s1<<endl<<s2<<endl;
+    int i=0,j=0;
+    while((s1[i]&&s2[j])!='\0'){
+        if(s1[i]==s2[j]){
+            i++;
+            j++;
+        }
+        else if(s1[i]>s2[j]){
+            while(s1[i]!=s2[j]){
+                j++;
+                b+=s2[j];
             }
-            else{
-                b+=s2[i];
-             }
-         }
-     }
-     cout<<a<<" "<<b;
-    // if(a>b){
-    //     cout<<1;
-    // }
-    // else if(a<b){
-    //     cout<<-1;
-    // }
-    // else{
-    //     cout<<0;
-    // }
+        }
+        else if(s1[i]<s2[j]){
+            while(s1[i]!=s2[j]){
+                i++;
+                a+=s2[j];
+            }
+        }
+    }
+    while((s1[i]=='\0')&&(s2[j]!='\0')){
+        int c1=0;
+        if(s1[i]==s2[j]){
+             if(c1>0){
+                b+=s2[j];
+            }
+            j++;
+            c1++;
+           
+        }
+       else{
+        b+=s2[j];
+        j++;
+       }
+    }
+    while((s1[i]!='\0')&&(s2[j]=='\0')){
+        int c2=0;
+        if(s1[i]==s2[j]){
+             if(c2>0){
+                a+=s1[i];
+            }
+            i++;
+            c2++;
+           
+        }
+       else{
+        a+=s1[i];
+        i++;
+       }
+    }
+     cout<<a<<" "<<b<<endl;
+    if(a>b){
+        cout<<1;
+    }
+    else if(a<b){
+        cout<<-1;
+    }
+    else{
+        cout<<0;
+    }
 }
