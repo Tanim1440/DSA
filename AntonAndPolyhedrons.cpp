75398c4cf43a7b4a1a208bc50e119e0514203994
Tanim1440 <mtanimkhan520@gmail.com>
2025-08-16 00:36:40 +0600

    66th dsa solve

---
+++ AntonAndPolyhedrons.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int ans=0;
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        if(str=="Tetrahedron")ans+=4;
+        else if(str=="Cube")ans+=6;
+        else if(str=="Octahedron")ans+=8;
+        else if(str=="Dodecahedron")ans+=12;
+        else if(str=="Icosahedron")ans+=20;
+    }
+    cout<<ans;
+}
