2234ae5dda07db40cfd88ac806a525c69efae511
Tanim1440 <mtanimkhan520@gmail.com>
2025-09-01 18:11:05 +0600

    86th dsa solve

---
+++ PaintingWithTwoColors.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        if(n%2==0){
+           if(b>a){
+            if(b%2==0){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+           }
+           else{
+            if(a%2==0 && b%2==0){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+           }
+        }
+        else{
+            if(b>a){
+                if(b%2!=0){
+                  cout<<"YES"<<endl;
+                }
+                else{
+                    cout<<"NO"<<endl;
+                }
+            }
+            else{
+                if(a%2!=0 && b%2!=0){
+                    cout<<"YES"<<endl;
+                }
+                else{
+                    cout<<"NO"<<endl;
+                }
+            }
+        }
+        
+    }
+}
