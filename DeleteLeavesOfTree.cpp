523568ebb93dc578e2f12cd150d4ea98d045ad7c
Tanim1440 <mtanimkhan520@gmail.com>
2026-07-25 15:50:17 +0600

    leetcode 1325 no solution

---
+++ DeleteLeavesOfTree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(root==NULL){
+            return NULL;
+        }
+       
+        TreeNode* left = removeLeafNodes(root->left,target);
+        TreeNode* right = removeLeafNodes(root->right,target);
+         if(left==NULL && right==NULL){
+            if(root->val==target){
+               return NULL;
+            }
+        }
+        root->left=left;
+        root->right=right;
+        return root;
+    }
+};
