b4d7e2021e4063e258afdc1446e71c394a05c474
Tanim1440 <mtanimkhan520@gmail.com>
2026-07-25 12:33:04 +0600

    leetcode 965 no solution

---
+++ UniValBinaryTree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool helper(TreeNode*root,int val){
+        if(root==NULL){
+            return true;
+        }
+        if(root->val!=val){
+            return false;
+        }
+        return helper(root->left,val) && helper(root->right,val);
+    }
+    bool isUnivalTree(TreeNode* root) {
+        return helper(root,root->val);
+    }
+};
